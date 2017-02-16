00644d6603e1833a98dbf606e0336620b7d38ce8
zhangqiang <zhangqiang@benbun.com>
2017-02-16 09:43:35 +0800

    update index

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,8 +1,51 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+
 class IndexController extends Controller {
-    public function index(){
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+
+    const DEFAULT_CNT = 5;
+    const DEFAULT_RESIDUE = 1;
+
+    public function index() {
+        try {
+            for ($i = 1;; $i++) {
+                if ($this->checkResidueCnt($i)) {
+                    $m1 = $this->getResidueCnt($i);
+                    if ($this->checkResidueCnt($m1)) {
+                        $m2 = $this->getResidueCnt($m1);
+                        if ($this->checkResidueCnt($m2)) {
+                            $m3 = $this->getResidueCnt($m2);
+                            if ($this->checkResidueCnt($m3)) {
+                                $m4 = $this->getResidueCnt($m3);
+                                if ($this->checkResidueCnt($m4)) {
+                                    $m5 = $this->getResidueCnt($m4);
+                                    if ($this->checkResidueCnt($m5)) {
+                                        echo $i;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
+    }
+
+    private function getResidueCnt($i) {
+        $res = $i - round($i / self::DEFAULT_CNT) - 1;
+        return $res;
+    }
+
+    private function checkResidueCnt($r) {
+        if ($r % self::DEFAULT_CNT == self::DEFAULT_RESIDUE) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
